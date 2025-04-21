5f12002e0d2b071bff374280255ca0169f70282f
Nicole <frega.nicole@gmail.com>
2025-04-21 23:19:18 +0200

    Added Home Manager configs

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,85 @@
+{ config, pkgs, ... }:
+
+{
+    nixpkgs = {
+      config = {
+        allowUnfree = true;
+        allowUnfreePredicate = (_: true);
+      };
+    };
+
+  home.username = "nicole";
+  home.homeDirectory = "/home/nicole";
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.packages = [
+  ];
+  
+  programs.home-manager.enable = true;
+  
+  gtk = {
+      enable = true;
+      font.name = "TeX Gyre Adventor 10";
+
+      theme = {
+        name = "rose-pine-moon";
+	package = pkgs.rose-pine-gtk-theme;
+      };
+      cursorTheme = {
+        name = "rose-pine-cursor";
+	package = pkgs.rose-pine-cursor;
+        size = 24;
+      };
+      #iconTheme = {
+      #  name = "rose-pine-moon";
+      #  package = pkgs.rose-pine-icon-theme;
+      #};
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.papirus-icon-theme;
+      };
+
+
+
+      gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+      gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+    
+  dconf = {
+    settings = {
+        "org/cinnamon/desktop/applications/terminal" = {
+            exec = "kitty";
+        };
+        "org/gnome/desktop/interface" = {
+            color-scheme = "prefer-dark";
+        };
+    };
+  };
+
+
+  xdg.desktopEntries.nemo = {
+    name = "Nemo";
+    exec = "${pkgs.nemo-with-extensions}/bin/nemo";
+  };
+  xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+          "inode/directory" = [ "nemo.desktop" ];
+          "application/x-gnome-saved-search" = [ "nemo.desktop" ];
+      };
+  };
+
+  xdg.configFile = {
+    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  };
+}
---
+++ .config/home-manager/theme.nix
@@ -0,0 +1,67 @@
+{ config, pkgs, ... }:
+
+{
+  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [
+    noto-fonts-emoji
+    dejavu_fonts
+    (nerdfonts.override {
+      fonts = [
+        "JetBrainsMono"
+        "Iosevka"
+      ];
+    })
+  ];
+
+  stylix = {
+    enable = true;
+
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+    };
+
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      monospace = {
+        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+
+      sizes = {
+        applications = 10;
+        terminal = 10;
+        desktop = 10;
+        popups = 10;
+      };
+    };
+
+    targets.nixvim.enable = false;
+    # targets.rofi.enable = false;
+
+    #FIXME: it doesnt work, it has to do with hyprpaper
+    image = ../../assets/Shogoki.png;
+
+    # opacity = {
+    #   desktop = 0.5;
+    #   terminal = 0.9;
+    # };
+  };
+}
