5d90dca6e74a4bb352327d6048e0827022249c07
Nicole <frega.nicole@gmail.com>
2025-10-29 16:03:25 +0100

    Added flake version of quickshell with idle support

--- .config/home-manager/dms.nix
+++ .config/home-manager/dms.nix
@@ -1,8 +1,4 @@
 {inputs, ...}: {
-  imports = [
-    inputs.dankMaterialShell.homeModules.dankMaterialShell.default
-  ];
-
   programs.dankMaterialShell = {
     enable = true;
     quickshell.package = inputs.quickshell.packages.x86_64-linux.default;
--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -2,7 +2,6 @@
   description = "Home Manager configuration of nicole";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -38,7 +37,7 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, stylix, dgop, dms-cli, dankMaterialShell, niri, quickshell, ... }:
+  outputs = inputs@{ nixpkgs, home-manager, stylix, dgop, dms-cli, dankMaterialShell, niri, quickshell, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -46,8 +45,6 @@
       homeConfigurations."nicole" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
         modules = [ 
         stylix.homeModules.stylix
         dankMaterialShell.homeModules.dankMaterialShell.default
@@ -56,9 +53,7 @@
         ./home.nix 
         ];
 
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+        extraSpecialArgs = { inherit inputs; };
       };
     };
 }
